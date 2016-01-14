0fa55a84f715eb9df993296a969a2ec4d1b87f07
Oleg Baranok <oleg.baranok@gmail.com>
2016-01-14 23:58:19 +0600

    Дабавлено изменение содержимого вкладок. Подправленны стили

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,39 @@
 var main = function () {
 	"use strict";
-	var tabNumber;
+	var toDos = [
+		"Закончить писать книгу",
+		"Вывести Грейси погулять в парк",
+		"Ответить на электронные письма",
+		"Подготовиться к лекции в понедельник",
+		"Обновить несколько новых задач",
+		"Купить продукты"
+	];
 	$(".tabs a span").toArray().forEach(function(element){
 		$(element).on("click", function () {
+			var $element = $(element),
+			$content;
 			$(".tabs span").removeClass("active");
-			$(element).addClass("active");
+			$element.addClass("active");
 			$("main .content").empty();
+			if ($element.parent().is(":nth-child(1)")) {
+				$content = $("<ul>");
+				for (var i = toDos.length-1; i >= 0; i--) {
+					$content.append($("<li>").text(toDos[i]));
+				}
+				$("main .content").append($content);
+			} else if ($element.parent().is(":nth-child(2)")) {
+				$content = $("<ul>");
+				toDos.forEach(function(todo) {
+					$content.append($("<li>").text(todo));
+				});
+				$("main .content").append($content);
+			} else if ($element.parent().is(":nth-child(3)")) {
+				$("main .content").append($("<input>"));
+				$("main .content").append($("<button>").text("+"));
+			}
 			return false;
 		});
 	});
+	$(".tabs a:first-child span").trigger("click");
 };
 $(document).ready(main);
