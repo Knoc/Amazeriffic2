cc667b004e5453830a7338dd81ea5fed2dbe012e
Oleg Baranok <oleg.baranok@gmail.com>
2016-02-04 01:42:30 +0600

    Добален JSON, добавленна вкладка теги, созданна функция orginizedByTags

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,8 @@
-var main = function () {
+var main = function (toDoObjects) {
 	"use strict";
-	var toDos = [
-		"Закончить писать книгу",
-		"Вывести Грейси погулять в парк",
-		"Ответить на электронные письма",
-		"Подготовиться к лекции в понедельник",
-		"Обновить несколько новых задач",
-		"Купить продукты"
-	];
+	var toDos = toDoObjects.map(function (toDo){
+		return toDo.description;
+	});
 	$(".tabs a span").toArray().forEach(function(element){
 		$(element).on("click", function () {
 			var $element = $(element),
@@ -28,12 +23,69 @@ var main = function () {
 				});
 				$("main .content").append($content);
 			} else if ($element.parent().is(":nth-child(3)")) {
-				$("main .content").append($("<input>"));
-				$("main .content").append($("<button>").text("+"));
+				 var organizedByTag = organizedByTags(toDoObjects); /*[
+					{
+						"name" : "покупки",
+						"toDos" : ["Купить продукты"]	
+					},
+					{
+						"name" : "рутина",
+						"toDos" : ["Купить продукты", "Вывести Грейси на прогулку в парк"]
+					},
+					{
+						"name" : "работа",
+						"toDos" : ["Сделать несколько новых задач","Приготовиться к лекции в понедельник",
+									"Ответить на электронные письма","Закончить писать книгу"]
+					}
+				]; */
+				organizedByTag.forEach (function (tag) {
+					var $tagName = $("<h3>").text(tag.name),
+						$content = $("<ul>");
+					tag.toDos.forEach (function (description) {
+						var $li = $("<li>").text(description);
+						$content.append($li);
+					});
+					$("main .content").append($tagName);
+					$("main .content").append($content);
+				});
+			} else if ($element.parent().is(":nth-child(4)")) {
+				var $input = $("<input>"),
+				$button = $("<button>").text("+");
+				$button.on("click", function () {
+					toDos.push($input.val());
+					$input.val("");
+				});
+				$content = $("<div>").append($input).append($button);
+				$("main .content").append($content);
 			}
 			return false;
 		});
 	});
 	$(".tabs a:first-child span").trigger("click");
 };
-$(document).ready(main);
+$(document).ready(function () {
+	$.getJSON("todos.json", function (toDoObjects) {
+		main (toDoObjects);
+	});
+});
+
+var organizedByTags = function (toDoObjects) {
+	var tags = [];
+	toDoObjects.forEach (function (toDo) {
+		toDo.tags.forEach (function (tag){
+			if (tags.indexOf(tag) === -1) {
+				tags.push(tag);
+			}
+		});
+	});
+	var tagObjects = tags.map (function (tag){
+		var toDoWithTag = [];
+		toDoObjects.forEach (function (toDo){
+			if (toDo.tags.indexOf(tag) !== -1) {
+				toDoWithTag.push(toDo.description);
+			}
+		});
+		return { "name" : tag, "toDos" : toDoWithTag };
+	});
+	console.log(tagObjects);
+}
